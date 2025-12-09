69d5358c3bcea1fdab29dc7c479a49c69c447997
Guilherme Ricardo de Paiva <guilhermepaiva2007h@gmail.com>
2025-12-09 13:53:44 -0300

    ponte

--- funcionamentoCodigo/ponte.py
+++ funcionamentoCodigo/ponte.py
@@ -2,12 +2,14 @@ import websockets
 import asyncio
 from funcionamentoCodigo.visaocomputacional.detectar import Aplicacao as ModoObjetos
 from funcionamentoCodigo.visaocomputacional.leitura import Aplicacao as ModoTexto
+from funcionamentoCodigo.neoview_piso.aplicacao import Aplicacao as ModoPiso
 
 ESP32_IP = '192.168.1.101'
 PORT = 8765
 
 modo_objetos = ModoObjetos()
 modo_texto = ModoTexto()
+modo_piso = ModoPiso()
 
 async def conectar_esp32():
     uri = f"ws://{ESP32_IP}:{PORT}"
@@ -28,6 +30,10 @@ async def conectar_esp32():
                 elif comando == "TEXTO":
                     print("Iniciando modo leitura de texto...")
                     modo_texto.executar()
+
+                elif comando == "PISO":
+                    print("Iniciando modo de mapeamento de piso...")
+                    modo_piso.executar()
 
                 else:
                     print(f"Comando desconhecido: {comando}")
